1242c170d4c4375c0c9cfe2c0f93f699fd7f22e4
Aayush Adhana <developeraayush145@gmail.com>
2024-06-18 17:47:45 +0530

    change in code

--- client/devAayushWeb/src/components/PostList.jsx
+++ client/devAayushWeb/src/components/PostList.jsx
@@ -8,39 +8,41 @@ import {Link} from 'react-router-dom';
 
 const PostList = () => {
     const [posts , setPosts] = useState([]);
-
+    const [loading, setLoading] = useState(true);
     // FETCHING DATA USING AXIOS
 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/posts').then(response => {
+      
+      const API_URL = process.env.REACT_API_URL || 'http://localhost:3000';
+        axios.get(`${API_URL}/api/posts`).then(response => {
             setPosts(response.data);
+            setLoading(false);
         }).catch((error) => {
             console.error('Error fetching posts' , error);
+            setLoading(false);
         })
     } , [])
 
     return(<>
        <div>
-   {/* {CircularProgress ? (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-          <CircularProgress /> {/* Display loading spinner */}
+            {loading ? (
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                    <CircularProgress />
+                </div>
+            ) : (
+                <Grid container spacing={1} justifyContent="center">
+                    {Array.isArray(posts) && posts.map(post => (
+                        <Grid item key={post._id} xs={12} sm={6} md={4} lg={3}>
+                            <CardComponent post={post}>
+                                <Link to={`/post/${post._id}`} style={{ textDecoration: 'none' }}>
+                                    <Button size="small">Read More</Button>
+                                </Link>
+                            </CardComponent>
+                        </Grid>
+                    ))}
+                </Grid>
+            )}
         </div>
-      {/* ) : ( */} 
-  <Grid container spacing={1} justifyContent="center">
-      {Array.isArray(posts) && posts.map(post => (
-        <Grid item key={post._id} xs={12} sm={6} md={4} lg={3}>
-          <CardComponent post={post} >
-            <Link to={`/post/${post._id}`} style={{textDecoration: 'none'}}>
-            <Button size="small">Read More</Button>
-            </Link>
-          </CardComponent>
-
-        </Grid>
-      ))}
-    </Grid>
-      {/* )} */}
-{/* </div> */}
-
     </>);
 }
 
