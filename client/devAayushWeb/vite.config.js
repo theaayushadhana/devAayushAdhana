995cbc3e992bb720386e45d9f2a66f869d75eecd
Aayush Adhana <developeraayush145@gmail.com>
2024-06-18 09:45:56 +0530

    vite config change

--- client/devAayushWeb/vite.config.js
+++ client/devAayushWeb/vite.config.js
@@ -1,17 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
   
+//   build: {
+//     outDir: 'dist', // Specify the output directory
+//     assetsDir: '.', // Serve HTML file directly from the root directory
+//     rollupOptions: {
+//       input: {
+//         main: '.././../client/index.js', // Specify your main entry JavaScript file
+//       },
+//     },
+//   },
+// })
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   build: {
-    outDir: 'dist', // Specify the output directory
-    assetsDir: '.', // Serve HTML file directly from the root directory
+    outDir: 'dist',
     rollupOptions: {
       input: {
-        main: '.././../client/index.js', // Specify your main entry JavaScript file
-      },
-    },
-  },
-})
+        main: '.././../client/index.js'  // Replace with your actual entry point
+      }
+    }
+  }
+});
+
