b87d1a278af2025e87e0b6aa5404770d000d85c7
Aayush Adhana <developeraayush145@gmail.com>
2024-06-18 12:09:03 +0530

    change are deployed

--- client/devAayushWeb/vite.config.js
+++ client/devAayushWeb/vite.config.js
@@ -41,16 +41,32 @@
 //   },
 // });
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+  
+//   build: {
+//     outDir: 'dist', // Specify the output directory
+   
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
   build: {
     outDir: 'dist', // Specify the output directory
-   
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'path', 'to', 'index.html'),
+
+      },
+    },
   },
 });
+
 
