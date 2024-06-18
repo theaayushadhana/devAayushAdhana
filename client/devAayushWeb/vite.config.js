2418a6980053e33d0ad9811abda440626db43101
Aayush Adhana <developeraayush145@gmail.com>
2024-06-18 08:52:51 +0530

    Initial commit

--- client/devAayushWeb/vite.config.js
+++ client/devAayushWeb/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  build: {
+    outDir: 'dist', // Specify the output directory
+    assetsDir: '.', // Serve HTML file directly from the root directory
+    rollupOptions: {
+      input: {
+        main: '.././../client/index.js', // Specify your main entry JavaScript file
+      },
+    },
+  },
 })
